07cb5e061bdca088881f096e5cfbafd7243fb947
ChristopherThorne714 <christhorne714@gmail.com>
2022-01-14 15:27:04 -0500

    ..

--- fizz buzz puzzle.js
+++ fizz buzz puzzle.js
@@ -1,18 +1,30 @@
 // take user input
 
-x = parseInt(prompt("Enter a number", 15));
+function fizzbuzz(x){
+    //var i;
+    // Create a for loop to iterate from 0 to the input
+    for (let i = 1; i <= x; i++) {
 
-var i;
+        //if the iterator is divisible by 15, then its divisible by both 5 and 3 as well, then print fizz buzz
+        if(i % 15 == 0) {
+            console.log("Fizz Buzz");
 
-for (i = 0; i >= x; i++) {
-    
-    if (i % 3 == 0) {
+        //if the current iteration is divisible by 3, print fizz
+        }else if (i % 3 == 0) {
+            console.log("Fizz");
+        
+        //If the current iteration is divisible by 5, print 
+        } else if(i % 5 == 0) {
+            console.log("Buzz");
+
+
+        //otherwise print i to the console
+        } else {
         console.log(i);
-
-    } else if(i % 5 == 0 && i % 3 == 0) {
-        console.log(i);
-
-    } else {
-    console.log(i);
+        }
     }
 }
+
+x = parseInt(prompt("Enter a number", 15));
+fizzbuzz(x);
+// I have no idea why this doesn't work. 
